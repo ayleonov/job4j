151290790ba8d7237288d0f5fb41b8d2b4b41881
ayleonov <allesh002@mail.ru>
2019-07-19 01:21:54 +0300

    3.3. Идеальный вес.[#157999]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static  double manWeight(double height){
+        return height - 100;
+    }
+
+    public static  double womanWeight(double height){
+        return height - 110;
+    }
+
+    public static void main(String[] args) {
+        double man = manWeight(190);
+        System.out.println("The perfect weight of man (with height : 190 sm) is: " + man + " kg");
+        double woman = womanWeight(170);
+        System.out.println("The perfect weight of woman (with height : 170 sm) is: " + woman + " kg");
+
+    }
+}
