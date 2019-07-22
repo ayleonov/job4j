3f41f314a0e049bd931dbf6406003983a20e156e
ayleonov <allesh002@mail.ru>
2019-07-22 11:32:16 +0300

    6.2. Перевернуть массив.[#157993] Доработка.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,37 +1,13 @@
 package ru.job4j.array;
 
 public class Turn {
-    // общий случай для четного и нечетного числа элементов
-    public int[] back(int[] array){
+    public int[] back(int[] array) {
         int temp;
-        for (int i=0;(array.length-i-i>1);i++){
+        for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = temp;
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
         return array;
     }
-
-    // вариант для нечетного числа элементов массива
-    public int[] backEven(int[] array){
-        int temp;
-        for (int i=0;(array.length-i-i>1);i++){
-            temp = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = temp;
-        }
-        return array;
-    }
-
-    // вариант для четного числа элементов массива
-    public int[] backOdd(int[] array){
-        int temp;
-        for (int i=0;(array.length-i-i>0);i++){
-            temp = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = temp;
-        }
-        return array;
-    }
-
 }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TurnTest {
-    // тестирование метода общего случая
     @Test
     public void when12345Then54321() {
         Turn turn = new Turn();
@@ -14,8 +13,6 @@ public class TurnTest {
         int[] result = turn.back(input);
         assertThat(result, is(new int[]{5, 4, 3, 2, 1}));
     }
-
-    // тестирование метода общего случая
     @Test
     public void when4162Then2614() {
         Turn turn = new Turn();
@@ -24,21 +21,12 @@ public class TurnTest {
         assertThat(result, is(new int[]{2, 6, 1, 4}));
     }
 
-    // тестирование метода для нечетного числа элементов массива
-    @Test
-    public void when12345Then54321Even() {
-        Turn turn = new Turn();
-        int[] input = new int[]{1, 2, 3, 4, 5};
-        int[] result = turn.backEven(input);
-        assertThat(result, is(new int[]{5, 4, 3, 2, 1}));
-    }
 
-    // тестирование метода для четного числа элементов массива
     @Test
-    public void when12345Then54321Odd() {
+    public void when4162311Then1132614() {
         Turn turn = new Turn();
-        int[] input = new int[]{4, 1, 6, 2};
+        int[] input = new int[]{4, 1, 6, 2, 3, 11};
         int[] result = turn.back(input);
-        assertThat(result, is(new int[]{2, 6, 1, 4}));
+        assertThat(result, is(new int[]{11, 3, 2, 6, 1, 4}));
     }
 }
