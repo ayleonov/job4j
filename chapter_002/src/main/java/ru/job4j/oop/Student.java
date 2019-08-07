38dbd43837a8576860a9943c226849df1cb90986
ayleonov <allesh002@mail.ru>
2019-08-07 15:23:58 +0300

    1.3. Музыкальный автомат

--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -1,8 +1,8 @@
 package ru.job4j.oop;
 
 public class Student {
-    private void music() {
-        System.out.println("Tra Tra Tra");
+    private void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 
     private void song() {
@@ -11,11 +11,8 @@ public class Student {
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
         petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe I can fly";
+        petya.music(song);
     }
 }
