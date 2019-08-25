9cd5ca218197b06fb9eb29924e055803fb6ec744
ayleonov <allesh002@mail.ru>
2019-08-25 16:10:53 +0300

    экзамен по ООП, задача

---
+++ chapter_002/src/main/java/ru/job4j/exam/Cofemachine.java
@@ -0,0 +1,42 @@
+package ru.job4j.exam;
+
+import java.lang.reflect.Array;
+
+public class Cofemachine {
+    private int[] changes(int value, int price) {
+        int[] deltacoins = new int[10];
+        int[] result;
+        int position = 0;
+        int deltatotal = value - price;
+        int[] coins = {1, 2, 5, 10};
+        int indexcoins = 3;
+
+        while (deltatotal > 0) {
+            if (deltatotal >= coins[indexcoins]) {
+                deltacoins[position] = coins[indexcoins];
+                if (deltatotal > coins[indexcoins]) {
+                    deltatotal -= coins[indexcoins];
+                } else {
+                    deltatotal = 0;
+                }
+            }
+            if (deltatotal <= coins[indexcoins]) {
+                indexcoins--;
+            }
+            position++;
+        }
+
+        result = new int[position];
+        System.arraycopy(deltacoins, 0, result, 0, position);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] output = new Cofemachine().changes(70, 35);
+        for (int i : output) {
+            System.out.println(i);
+        }
+    }
+}
