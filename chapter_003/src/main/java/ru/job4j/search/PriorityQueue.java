4ade8ca1f0f2c287c8a9c1db92eaa91909e11c3e
ayleonov <allesh002@mail.ru>
2019-08-27 17:29:45 +0300

    2. Очередь с приоритетом на LinkedList[#158037]

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,24 +1,20 @@
 package ru.job4j.search;
 
 import java.util.LinkedList;
-import java.util.ListIterator;
+
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
-    ListIterator iterator = tasks.listIterator();
-    Task currentTask;
 
     public void put(Task task) {
         int index = 0;
         int currentPriority = task.getPriority();
         if (tasks.size() > 0) {
-            for (int i = 0; i < tasks.size(); i++) {
-                for (Task currentTask : tasks)
-                    if (currentPriority < (currentTask.getPriority())) {
-
-                        index = tasks.indexOf(currentTask);
-                        break;
-                    }
+            for (Task currentTask : tasks) {
+                if (currentPriority < (currentTask.getPriority())) {
+                    index = tasks.indexOf(currentTask);
+                    break;
+                }
             }
 
         }
