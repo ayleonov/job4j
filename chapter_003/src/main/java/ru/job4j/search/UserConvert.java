a715dd778041befb3eebdb8138f11695e12165df
ayleonov <allesh002@mail.ru>
2019-08-28 16:36:18 +0300

    2. Написать программу преобразования List в Map.[#158044]

---
+++ chapter_003/src/main/java/ru/job4j/search/UserConvert.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.HashMap;
+
+public class UserConvert {
+    private static final Random RN = new Random();
+    private List<Integer> listIds;
+
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> map = new HashMap<>();
+        listIds = new ArrayList();
+        for (User element : list) {
+            int id = getId();
+
+            map.put(id, element);
+            listIds.add(id);
+        }
+        return map;
+    }
+
+    public Integer getId() {
+        return (int) (System.currentTimeMillis() + RN.nextInt());
+    }
+
+    public List<Integer> getListids() {
+        return listIds;
+    }
+}
+
+
---
+++ chapter_003/src/main/java/ru/job4j/search/UserConvert2.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class UserConvert2 {
+    private static final Random RN = new Random();
+
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> map = new HashMap<>();
+        for (User element : list) {
+            map.put(getId(), element);
+        }
+        return map;
+    }
+
+    public Integer getId() {
+        return (int) (System.currentTimeMillis() + RN.nextInt());
+    }
+}
+
+
