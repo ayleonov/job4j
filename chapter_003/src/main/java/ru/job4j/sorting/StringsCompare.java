d78c3c7d85179f769830d69647f53eb0b6a97f50
ayleonov <allesh002@mail.ru>
2019-08-31 10:48:22 +0300

    3. Компаратор для строк.[#158027]

--- chapter_003/src/main/java/ru/job4j/sorting/StringsCompare.java
+++ chapter_003/src/main/java/ru/job4j/sorting/StringsCompare.java
@@ -10,22 +10,23 @@ public class StringsCompare implements Comparator<String> {
         char[] arrright = right.toCharArray();
         int delta = arrleft.length - arrright.length;
         int lenghtMin = delta <= 0 ? arrleft.length : arrright.length;
-        int strcompare = 0;
+        int cyclestatus = 0;
 
         for (int i = 0; i < lenghtMin; i++) {
-            strcompare = Character.compare(arrleft[i], arrright[i]);
             if (arrleft[i] != arrright[i]) {
-                result = strcompare;
+                result = Character.compare(arrleft[i], arrright[i]);;
+                cyclestatus = 1;
                 break;
             }
         }
-        if (delta == 0) {
-            result = 0;
-        } else {
-            result = delta > 0 ? -1 : 1;
+
+        if (cyclestatus != 1) {
+            if (delta == 0) {
+                result = 0;
+            } else {
+                result = delta > 0 ? 1 : -1;
+            }
         }
-
-
         return result;
     }
 }
