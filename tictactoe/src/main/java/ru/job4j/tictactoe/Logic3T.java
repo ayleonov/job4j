1d73ea19e431ed0ae06d777b89199aa4418eff25
ayleonov <allesh002@mail.ru>
2019-09-18 17:42:34 +0300

    Крестики-нолики на JavaFX[#157939]

---
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,91 @@
+package ru.job4j.tictactoe;
+
+import java.util.function.Predicate;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean isWinnerX() {
+        return (table[0][0].hasMarkX() && table[1][0].hasMarkX() && table[2][0].hasMarkX())
+                || (table[0][1].hasMarkX() && table[1][1].hasMarkX() && table[2][1].hasMarkX())
+                || (table[0][2].hasMarkX() && table[1][2].hasMarkX() && table[2][2].hasMarkX())
+                || (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkX())
+                || (table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkX())
+                || (table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkX())
+                || (table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX())
+                || (table[2][0].hasMarkX() && table[1][1].hasMarkX() && table[0][2].hasMarkX());
+    }
+
+
+    public boolean isWinnerO() {
+        return (table[0][0].hasMarkO() && table[1][0].hasMarkO() && table[2][0].hasMarkO())
+                || (table[0][1].hasMarkO() && table[1][1].hasMarkO() && table[2][1].hasMarkO())
+                || (table[0][2].hasMarkO() && table[1][2].hasMarkO() && table[2][2].hasMarkO())
+                || (table[0][0].hasMarkO() && table[0][1].hasMarkO() && table[0][2].hasMarkO())
+                || (table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkO())
+                || (table[2][0].hasMarkO() && table[2][1].hasMarkO() && table[2][2].hasMarkO())
+                || (table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO())
+                || (table[2][0].hasMarkO() && table[1][1].hasMarkO() && table[0][2].hasMarkO());
+
+    }
+
+    public boolean isWinnerX2() {
+        return
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                        || this.fillBy(Figure3T::hasMarkX, 2, 0, -1, 1);
+    }
+
+    public boolean isWinnerO2() {
+        return
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0)
+                        || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
+                        || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                        || this.fillBy(Figure3T::hasMarkO, 2, 0, -1, 1);
+    }
+
+    public boolean hasGap() {
+        boolean res = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (table[i][j].hasMarkX()) {
+                    continue;
+                }
+                if (table[i][j].hasMarkO()) {
+                    continue;
+                } else {
+                    res = true;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean res = true;
+        for (int i = 0; i < this.table.length; i++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+}
