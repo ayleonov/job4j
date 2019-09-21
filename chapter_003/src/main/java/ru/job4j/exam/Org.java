f8c46d43216483a092be6ec4b12ba5035c4719ed
ayleonov <allesh002@mail.ru>
2019-09-21 13:23:23 +0300

    Контрольные вопросы.[#158034]

--- chapter_003/src/main/java/ru/job4j/exam/Org.java
+++ chapter_003/src/main/java/ru/job4j/exam/Org.java
@@ -3,44 +3,35 @@ package ru.job4j.exam;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
 public class Org {
     private List<String> namestring = new ArrayList<>();
+
     public List<String> getNamestring() {
         return namestring;
     }
-    public Org() {
-        namestring.add("K1");
-        namestring.add("K2");
-        namestring.add("K1\\SK1");
-        namestring.add("K1\\SK2");
-        namestring.add("K2\\SK1");
-        namestring.add("K2\\SK2");
-        namestring.add("K1\\SK1\\SSK1");
-        namestring.add("K1\\SK1\\SSK2");
-        namestring.add("K1\\SK2\\SSK1");
-        namestring.add("K1\\SK2\\SSK2");
-        namestring.add("K2\\SK1\\SSK1");
-        namestring.add("K2\\SK1\\SSK2");
-        namestring.add("K2\\SK2\\SSK1");
-        namestring.add("K2\\SK2\\SSK2");
+
+    public Org(List<String> namestring) {
+        this.namestring = namestring;
     }
 
     public void sorting(int directionSorting) {
         namestring.sort(directionSorting == 0 ? directSorting : reverseSorting);
     }
+
     Comparator directSorting = new Comparator() {
         @Override
         public int compare(Object o1, Object o2) {
-            String str1 = (String)o1;
-            String str2 = (String)o2;
+            String str1 = (String) o1;
+            String str2 = (String) o2;
             return str1.compareTo(str2);
         }
     };
     Comparator reverseSorting = new Comparator() {
         @Override
         public int compare(Object o1, Object o2) {
-            String str1 = (String)o1;
-            String str2 = (String)o2;
+            String str1 = (String) o1;
+            String str2 = (String) o2;
 
             return str2.compareTo(str1);
         }
--- chapter_003/src/test/java/ru/job4j/exam/OrgTest.java
+++ chapter_003/src/test/java/ru/job4j/exam/OrgTest.java
@@ -1,23 +1,46 @@
 package ru.job4j.exam;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+
 public class OrgTest {
+    List<String> list;
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<>();
+        list.add("K1");
+        list.add("K2");
+        list.add("K1\\SK1");
+        list.add("K1\\SK2");
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K2\\SK1\\SSK2");
+        list.add("K2\\SK1");
+    }
+
     @Test
     public void whenDirectSorting() {
-        Org firstSorting = new Org();
+
+        Org firstSorting = new Org(list);
         firstSorting.sorting(0);
         List<String> result = firstSorting.getNamestring();
         String[] expect = {"K1", "K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1",
-                "K1\\SK1\\SSK2", "K2","K2\\SK1\\SSK1", "K2\\SK1\\SSK2",
+                "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2",
                 "K2\\SK1"};
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenReverseSorting() {
-        Org firstSorting = new Org();
+        Org firstSorting = new Org(list);
         firstSorting.sorting(0);
         List<String> result = firstSorting.getNamestring();
         String[] expect = {"K2", "K2\\SK1", "K2\\SK1\\SSK2", "K2\\SK1\\SSK1",
