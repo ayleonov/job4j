d49ff254916c9fe7f0be1ec2bdcdca9523dbe199
ayleonov <allesh002@mail.ru>
2019-09-27 11:44:41 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#180431]

---
+++ chapter_005/src/main/java/ru/job4j/generics/SimpleStack.java
@@ -0,0 +1,82 @@
+package ru.job4j.generics;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleStack<E> implements Iterable<E> {
+    private Node<E> node;
+    private int modCount = 0;
+    private Node<E> first;
+    private int size;
+
+    public void push(E value) {
+        Node<E> newLink = new Node(value);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    public E poll() {
+      Node<E> deleted = this.first;
+        this.first = this.first.next;
+        this.size--;
+        return deleted.data;
+    }
+
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    public int countelements() {
+        int numberelements = 0;
+        for (int i = 0; i < size; i++) {
+            if (get(i) != null) {
+                numberelements++;
+            }
+        }
+        return numberelements;
+    }
+
+    @Override
+    public Iterator iterator() {
+        int expectedmodCount = modCount;
+        int numberelements = this.countelements();
+
+        return new Iterator() {
+            int iterposition = 0;
+
+
+            @Override
+            public boolean hasNext() {
+
+                return iterposition < numberelements;
+            }
+
+            @Override
+            public Object next() {
+                if (expectedmodCount != modCount) {
+                    throw new ConcurrentModificationException();
+                } else if (!(hasNext())) {
+                    throw new NoSuchElementException();
+                } else {
+                    return get(iterposition++);
+                }
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
+
---
+++ chapter_005/src/test/java/ru/job4j/generics/SimpleStackTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.generics;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SimpleStackTest {
+    private SimpleStack<Integer> dincont;
+
+    @Before
+    public void beforeTest() {
+        dincont = new SimpleStack<>();
+        dincont.push(1);
+        dincont.push(2);
+    }
+
+    @Test
+    public void whenAddTenElementsThenItInContainer() {
+
+        dincont.push(3);
+        dincont.push(4);
+        dincont.push(5);
+
+        assertThat(dincont.get(0), is(5));
+        assertThat(dincont.get(1), is(4));
+
+    }
+
+    @Test
+    public void whenAddTwoElementsThenGetFirstByIndex() {
+        assertThat(dincont.get(1), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenTestingHasNextAndNext() {
+        Iterator it = dincont.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void whenDeletedAndReceiveSecondElement() {
+        dincont.push(3);
+        assertThat(dincont.poll(), is(3));
+    }
+}
