e970ffbbb56dd208bb568be21b824663c5bb6308
ayleonov <allesh002@mail.ru>
2019-09-30 17:39:23 +0300

    1. Реализовать коллекцию Set на массиве[#180458]

---
+++ chapter_005/src/test/java/ru/job4j/generics/SimpleSetTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+
+public class SimpleSetTest {
+    @Test
+    public void whenTryAddNewElement() {
+        SimpleSet<Integer> sset = new SimpleSet();
+        boolean result = sset.add(14);
+
+        assertThat(sset.sa.getArray()[0], is(14));
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenTryAddDublicat() {
+        SimpleSet<Integer> sset = new SimpleSet();
+        sset.add(14);
+        boolean result = sset.add(14);
+        assertThat(sset.sa.getArray()[0], is(14));
+        assertThat(result, is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenTestingIterator() {
+        SimpleSet<Integer> sset = new SimpleSet();
+        Iterator it = sset.iterator();
+        sset.add(14);
+        sset.add(7);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(14));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+}
