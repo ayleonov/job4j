ac19a7f79e0bddb95f8f885213cb17c12e64f96f
ayleonov <allesh002@mail.ru>
2019-10-31 17:21:27 +0300

    [#180478]

---
+++ chapter_006/src/main/java/ru/job4j/socket/Client.java
@@ -0,0 +1,55 @@
+package ru.job4j.socket;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        int servPort = 5000;
+        String ipAddress = "127.0.0.1";
+        try {
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+            System.out.println("Подключаемся к серверу:" + servPort);
+            Socket socket = new Socket(inetAddress, servPort);
+            InputStream socketInputStream = socket.getInputStream();
+            OutputStream socketOutputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(socketInputStream);
+            DataOutputStream out = new DataOutputStream(socketOutputStream);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String string = null;
+            System.out.println("Введите фразу для передачи серверу:");
+            while (true) {
+                string = reader.readLine();
+                out.writeUTF(string);
+                out.flush();
+                string = in.readUTF();
+                System.out.println("Сервер прислал в ответ:" + string);
+                System.out.println("Введите фразу для отправки на сервер:");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ chapter_006/src/main/java/ru/job4j/socket/Server.java
@@ -0,0 +1,37 @@
+package ru.job4j.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 5000;
+        try {
+            ServerSocket servSocket = new ServerSocket(port);
+            System.out.println("Ждем подключения к серверу");
+            Socket socket = servSocket.accept();
+            System.out.println("Подключиение состоялось");
+
+            InputStream socketInputStream = socket.getInputStream();
+            OutputStream socketOutputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(socketInputStream);
+            DataOutputStream out = new DataOutputStream(socketOutputStream);
+            String string = null;
+            while (true) {
+                string = in.readUTF();
+                System.out.println("Мы получили следующее сообщение:" + string);
+                System.out.println("Отправка обратно");
+                out.writeUTF(string);
+                out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
