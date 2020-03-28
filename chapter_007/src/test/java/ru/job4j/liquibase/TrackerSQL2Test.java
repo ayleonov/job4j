7b0fb29661d03134ab57d48ad1e3813f6115e6e0
ayleonov <allesh002@mail.ru>
2020-03-28 15:31:14 +0300

    4.5. Travis CI и PostgreSQL[#180404]

--- chapter_007/src/test/java/ru/job4j/liquibase/TrackerSQL2Test.java
+++ chapter_007/src/test/java/ru/job4j/liquibase/TrackerSQL2Test.java
@@ -16,12 +16,15 @@ public class TrackerSQL2Test {
     @Test
     public void createItem() throws ParseException {
         TrackerSQL tracker = new TrackerSQL();
+        tracker.init();
+        int numberItemsBeforeCreate = tracker.findByName("name").size();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
         String timeStr = "2020-09-01";
         Date date = df.parse(timeStr);
         long time = date.getTime();
         tracker.add(new Item("name", "desc", time ));
-        assertThat(tracker.findByName("name").size(), is(1));
+        int numberItemsafterCreate = tracker.findByName("name").size();
+        assertThat(numberItemsafterCreate-numberItemsBeforeCreate, is(1));
     }
 }
