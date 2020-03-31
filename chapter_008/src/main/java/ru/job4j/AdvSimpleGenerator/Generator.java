a3d69e6bee82173aace47c759760ec90d1ed3b29
ayleonov <allesh002@mail.ru>
2020-03-31 17:00:01 +0300

    1. Исправить код класс SimpleGenerator.[#180502] Рефакторинг.

--- chapter_008/src/main/java/ru/job4j/AdvSimpleGenerator/Generator.java
+++ chapter_008/src/main/java/ru/job4j/AdvSimpleGenerator/Generator.java
@@ -8,27 +8,21 @@ public class Generator implements IGenerator {
     @Override
     public String produce(String template, Map<String, String> args) {
 
-        String textBefore;
-        String textAfter;
-        String templ;
-        String newText;
         String res = template;
+
         if (args.size() == 0) {
             throw new NoSuchElementException();
         }
         for (Map.Entry<String, String> pair : args.entrySet()) {
 
             int indexBeginSearchingWord = res.indexOf(pair.getKey());
-            if (indexBeginSearchingWord > -1) {
-                textBefore = res.substring(0, indexBeginSearchingWord);
-                templ = pair.getKey();
-                newText = pair.getValue();
-                int lengthTempl = templ.length();
-                textAfter = template.substring(indexBeginSearchingWord + lengthTempl);
-                res = textBefore + newText + textAfter;
-            } else {
+
+            if (indexBeginSearchingWord <= -1) {
                 throw new NoSuchElementException();
             }
+
+            res = res.substring(0, indexBeginSearchingWord) + pair.getValue() +
+                    res.substring(indexBeginSearchingWord + pair.getKey().length());
         }
         return res;
     }
